1dde374cd35acb8dc1d879b7024bc6ec37ad7a87
a14rathore <87847257+a14rathore@users.noreply.github.com>
2021-10-31 19:12:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import AboutUs from "./Components/AboutUs";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/Textform";
 import React, { useState } from 'react'
+import Alert from "./Components/alert";
 function App() {
   const [mode,setDarkMode]=useState('light');
+  const [alert, setalert]=useState(null);
+  const showalert =(message,type)=>{
+    setalert({
+      msg:message,
+      type:type
+
+    })
+    setTimeout(() => {
+      setalert(null);
+    },1500);
+
+  }
   const toggleMode=()=>{
     if(mode==='light'){
       setDarkMode('dark');
       document.body.style.backgroundColor='#042743';
+      showalert("Dark mode has been active", "success");
     }else{
       setDarkMode('light');
       document.body.style.backgroundColor='white';
-
+      showalert("Light mode has been active", "success");
     }
-
   }
   return (
     <>
       <Navbar Title="Textutils" About="About Us" mode={mode} toggleMode={toggleMode}></Navbar>
-      <TextForm Heading="Enter Text in below Area" mode={mode}/>
+      <Alert alert={alert}/>
+      <TextForm Heading="Enter Text in below Area" mode={mode} showalert={showalert}/>
       {/* <AboutUs/> */}
     </>
   );
--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -5,11 +5,13 @@ export default function Textform(props) {
   const handleUpperCase = () => {
     let newText = text.toUpperCase();
     setText(newText);
+    props.showalert("converted to UpperCase","success");
   }
 
   const handleLowerCase = () => {
     let newText = text.toLowerCase();
     setText(newText);
+    props.showalert("converted to LowerCase","success");
   }
 
   const handleOnChange = (event) => {
@@ -38,15 +40,24 @@ export default function Textform(props) {
     }
     reword=newText.join(' ');
     setText(reword);
+    props.showalert("reverse word","success");
   }
   
   const revText = () => {
     let newText = text.split('').reverse().join('');
     setText(newText);
+    props.showalert("reverse Text","success");
   }
   const removeWhiteSpace=()=>{
     let newText=text.split(' ').filter(s => s).join(' ');
     setText(newText);
+  }
+  const copy =()=>{
+    let text=document.getElementById("exampleFormControlTextarea1");
+    text.select();
+    text.setSelectionRange(0,9999);
+    navigator.clipboard.writeText(text.value);
+    props.showalert("Text Copied","success");
   }
   return (
     <>
@@ -63,7 +74,8 @@ export default function Textform(props) {
         <button className="btn btn-secondary mx-3" onClick={revWords}>Reverse Words</button>
         <button className="btn btn-warning mx-3" onClick={revText}>Reverse Text</button>
         <button className="btn btn-info mx-3" onClick={removeWhiteSpace}>Remove extra White space</button>
-        <button className="btn btn-danger mx-3" onClick={clearText}>Clear</button>
+        <button className={`btn btn-${props.mode==='dark'?'light':'dark'} mx-3`} onClick={copy}>copy text</button>
+        <button className="btn btn-danger my-3" onClick={clearText}>Clear</button>
       </div>
       <div className="container my-4" style={{color:props.mode==='dark'?'white':'#042743'}}>
         <h2>Your text summary</h2>
