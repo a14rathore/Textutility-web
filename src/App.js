2279dc94e7539026b27632e827b9eee5876bc71a
a14rathore <87847257+a14rathore@users.noreply.github.com>
2021-10-31 20:23:43 +0530

    f

--- src/App.js
+++ src/App.js
@@ -3,37 +3,52 @@ import Navbar from "./Components/Navbar";
 import TextForm from "./Components/Textform";
 import React, { useState } from 'react'
 import Alert from "./Components/alert";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
-  const [mode,setDarkMode]=useState('light');
-  const [alert, setalert]=useState(null);
-  const showalert =(message,type)=>{
+  const [mode, setDarkMode] = useState('light');
+  const [alert, setalert] = useState(null);
+  const showalert = (message, type) => {
     setalert({
-      msg:message,
-      type:type
+      msg: message,
+      type: type
 
     })
     setTimeout(() => {
       setalert(null);
-    },1500);
+    }, 1500);
 
   }
-  const toggleMode=()=>{
-    if(mode==='light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setDarkMode('dark');
-      document.body.style.backgroundColor='#042743';
+      document.body.style.backgroundColor = '#042743';
       showalert("Dark mode has been active", "success");
-    }else{
+    } else {
       setDarkMode('light');
-      document.body.style.backgroundColor='white';
+      document.body.style.backgroundColor = 'white';
       showalert("Light mode has been active", "success");
     }
   }
   return (
     <>
-      <Navbar Title="Textutils" About="About Us" mode={mode} toggleMode={toggleMode}></Navbar>
-      <Alert alert={alert}/>
-      <TextForm Heading="Enter Text in below Area" mode={mode} showalert={showalert}/>
-      {/* <AboutUs/> */}
+    <Router>
+    <Navbar Title="Textutils" About="About Us" mode={mode} toggleMode={toggleMode}></Navbar>
+      <Alert alert={alert} />
+      <Switch>
+        <Route path="/about">
+          <AboutUs/>
+        </Route>
+        <Route path="/">
+          <TextForm Heading="Enter Text in below Area" mode={mode} showalert={showalert} />
+        </Route>
+      </Switch>
+    </Router>
+      
     </>
   );
 }
